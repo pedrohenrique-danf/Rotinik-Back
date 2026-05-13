40d5d445722fff816b514dbc5c0f0ec64e764e96
pedroivo1 <pedroivoal1@gmail.com>
2026-05-13 08:44:04 -0300

    fix(DTOs): some objects nedeed to be "required"

--- RotinikApi/DTOs/Requests/User/UserCreateRequest.cs
+++ RotinikApi/DTOs/Requests/User/UserCreateRequest.cs
@@ -2,9 +2,9 @@ namespace RotinikApi.DTOs.Requests
 {
     public class UserCreateRequest
     {
-        public string Name { get; set; }
-        public string Email { get; set; }
-        public string Phone { get; set; }
-        public string Password { get; set; }
+        public required string Name { get; set; }
+        public required string Email { get; set; }
+        public required string Phone { get; set; }
+        public required string Password { get; set; }
     }
 }
