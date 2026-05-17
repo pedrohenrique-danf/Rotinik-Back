ab67ffbbd66f6d67c15778c9bacd4e8992609c74
pedroivo1 <pedroivoal1@gmail.com>
2026-05-16 23:08:10 -0300

    feat: async Controller and Service

--- src/Models/User.cs
+++ src/Models/User.cs
@@ -1,30 +1,12 @@
-using System.ComponentModel.DataAnnotations;
-
 namespace Rotinik.Models;
 
 public class User
 {
-    [Key]
     public int Id { get; set; }
-
-    [Required]
-    [MaxLength(100)]
     public string Name { get; set; } = string.Empty;
-
-    [Required]
     public DateTime BirthDate { get; set; }
-
-    [Required]
-    [MaxLength(50)]
     public string UserName { get; set; } = string.Empty;
-
-    [Required]
     public string Password { get; set; } = string.Empty;
-
-    [Required]
-    [EmailAddress]
     public string Email { get; set; } = string.Empty;
-
-    [Required]
     public bool isPremium { get; set; } = false;
 }
--- src/Services/IUserService.cs
+++ src/Services/IUserService.cs
@@ -4,10 +4,10 @@ namespace Rotinik.Services;
 
 public interface IUserService
 {
-    void CreateUser(UserRegistrationDto dto);
-    UserProfileDto? GetPublicProfile(string username);
-    void UpdateUser(int id, int currentUserId, UserUpdateDto dto);
-    void DeleteUser(int id, int currentUserId);
-    string? Login(UserLoginDto dto);
-    UserResponseDto? GetCurrentUser(int userId);
+    Task CreateUserAsync(UserRegistrationDto dto);
+    Task<UserProfileDto?> GetPublicProfileAsync(string username);
+    Task UpdateUserAsync(int id, int currentUserId, UserUpdateDto dto);
+    Task DeleteUserAsync(int id, int currentUserId);
+    Task<string?> LoginAsync(UserLoginDto dto);
+    Task<UserResponseDto?> GetCurrentUserAsync(int userId);
 }
